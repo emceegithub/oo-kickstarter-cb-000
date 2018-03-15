0121a48950fad2bc4b39dbb93a796cd384ac2560
Mark Cruz <markchriscruz@yahoo.com>
2018-03-15 22:25:21 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,13 @@
 class Project
-  attr_reader :title
+  attr_reader :title, :backers
   
   def initialize(title)
     @title = title
+    @backers = []
   end
   
   def add_backer(backer)
     @backers << backer
-    backer.back_project(self)
+    backer.backed_project(self)
   end
 end
